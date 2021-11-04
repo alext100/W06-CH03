1f65538069e3182f0b47059b0105b7a521deb19f
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-04 22:45:22 +0100

    router.delete() method that delete a question by id from DB added

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 require("dotenv").config();
+// const inquirer = require("inquirer");
 require("./database/index");
 
 const initializeServer = require("./server/index");
 
 const port = process.env.SERVER_PORT || 5000;
+
+/* (async () => {
+  const answers = await inquirer.prompt([
+    {
+      name: "puerto",
+      type: "input",
+      message: "¿En qué puerto quieres que se inicie la API?",
+      default: "4000",
+    },
+    {
+      name: "databases",
+      type: "list",
+      message: "¿Qué base de datos quieres usar?",
+      choices: [
+        {
+          name: "PruebasDB",
+          value: "Pruebas",
+        },
+        {
+          name: "ProduccionDB",
+          value: "Produccion",
+        },
+      ],
+      default: "Pruebas",
+    },
+    {
+      name: "modificardb",
+      type: "list",
+      message:
+        "¿Quieres permitir que los clientes puedan crear, borrar y modificar?",
+      choices: [
+        {
+          name: "Permitir",
+          value: "Si",
+        },
+        {
+          name: "NoPermitir",
+          value: "No",
+        },
+      ],
+      default: "No",
+    },
+  ]);
+  console.log(answers);
+})(); */
 
 initializeServer(port);
--- server/routes/preguntasRoutes.js
+++ server/routes/preguntasRoutes.js
@@ -28,7 +28,7 @@ router.get("/things/:id", async (req, res, next) => {
 router.post(
   "/new",
   (req, res, next) => {
-    console.log("Están creando una pregunta!");
+    console.log("Están creando una Thing!");
     next();
   },
   async (req, res, next) => {
@@ -38,7 +38,25 @@ router.post(
       res.json(newQuestion);
     } catch (error) {
       error.code = 400;
-      error.message = "Error in POST new question!";
+      error.message = "Error in POST new Thing!";
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  "/things/:id",
+  async (req, res, next) => {
+    console.log("Están borrando una Thing with id: ", req.params.id);
+    next();
+  },
+  async (req, res, next) => {
+    try {
+      const thigToDelete = await Things.findByIdAndRemove(req.params.id);
+      res.json(thigToDelete);
+    } catch (error) {
+      error.code = 500;
+      error.message = "Error on Delete Thing!";
       next(error);
     }
   }
