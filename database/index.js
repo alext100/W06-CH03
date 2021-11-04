35d82961e6687b71b62a4d5142d90b2f7eb4bd39
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-04 23:52:00 +0100

    To mongoose.connect was added condition to choose DB

--- database/index.js
+++ database/index.js
@@ -3,11 +3,20 @@ const debug = require("debug")("things:database");
 const chalk = require("chalk");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_PRUEBAS, (error) => {
-  if (error) {
-    debug(chalk.red("No se ha podido iniciar la base de datos."));
-    debug(chalk.red(error.message));
-    return;
-  }
-  debug(chalk.green("Conectado a la base de datos"));
-});
+const mongoDBPruebas = process.env.MONGODB_PRUEBAS;
+const mongoDBProduction = process.env.MONGODB_PRODUCTION;
+
+const mongodbConnect = (dataBase) =>
+  mongoose.connect(
+    dataBase === "mongoDBPruebas" ? mongoDBPruebas : mongoDBProduction,
+    (error) => {
+      if (error) {
+        debug(chalk.red("No se ha podido iniciar la base de datos."));
+        debug(chalk.red(error.message));
+        return;
+      }
+      debug(chalk.green("Conectado a la base de datos: ", dataBase));
+    }
+  );
+
+module.exports = mongodbConnect;
